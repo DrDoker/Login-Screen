1eba238aa62dcc80390b49da76919c2091879cb1
Serhii Tkachenko <doker.tk@gmail.com>
2022-08-22 23:24:04 +0200

    Small fix

--- Login Screen/Sources/App/SceneDelegate.swift
+++ Login Screen/Sources/App/SceneDelegate.swift
@@ -14,12 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = LoginViewController()
-        window.makeKeyAndVisible()
-        self.window = window
-
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = LoginViewController()
+        window?.makeKeyAndVisible()
     }
-
 }
 
